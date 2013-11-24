b403ada3e3a226017508572cd742ee177c828a14
Scott Enriquez <scott.c.enriquez@gmail.com>
2013-11-23 18:43:49 -0600

    initial commit

---
+++ WikiDepth.py
@@ -0,0 +1,19 @@
+import sys, wikipedia
+from DepthSearch import *
+
+def wikiDepth(reader, writer):
+	for pair in parser(reader):
+		"""
+		parse inputs, handle initial error checking, and kick off search
+		"""
+		userStartPage = wikipedia.page(pair[1])
+		initialDepthRow = userStartPage.links
+		userTargetPage = wikipedia.page(pair[3])
+		pagesSearched = {}
+		initialDepth = 0
+		depthSearch(userStartPage, initialDepthRow, userTargetPage, pagesSearched, initialDepth)
+
+def parser(reader):
+	return (map(str, line.split("\"")) for line in reader)
+
+wikiDepth(sys.stdin, sys.stdout)
